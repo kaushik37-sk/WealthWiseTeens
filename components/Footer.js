a04e20f38e051e3986e7cd9f6ddcd193312d3e30
kaushik37-sk <7lowkeykaushik@gmail.com>
2025-04-26 19:57:10 -0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-light text-center text-lg-start mt-5 py-3">
+      <div className="container">
+        <span className="text-muted">© {new Date().getFullYear()} WealthWiseTeenz. All rights reserved.</span>
+      </div>
+    </footer>
+  );
+}
